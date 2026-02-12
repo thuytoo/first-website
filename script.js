d6e1f2c927e068033a1e77ac58ac31749a3dc42e
Thuy To <thuytoussh@gmail.com>
2026-02-12 15:33:10 +0200

    Update script.js
    use a localStorage feature so that when a user closes and reopens a website, the site remembers that they are in Dark Mode

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 const button = document.getElementById('colorButton');
 const body = document.body;
 
+// 1. As soon as the website finishes loading, check what the user selected last time
+const currentTheme = localStorage.getItem('theme');
+
+if (currentTheme === 'dark') {
+    body.classList.add('dark-mode');
+    button.textContent = "Switch to Light Mode";
+}
+
+// 2. Handling events when a button is pressed
 button.addEventListener('click', () => {
-    // Each time the button is pressed, it will add or remove the "dark-mode" class from the body tag
     body.classList.toggle('dark-mode');
     
-    // Change the text on the button so users know it
+    let theme = 'light'; // The default is light
+    
     if (body.classList.contains('dark-mode')) {
+        theme = 'dark';
         button.textContent = "Switch to Light Mode";
     } else {
         button.textContent = "Switch to Dark Mode";
     }
+    
+    // 3. Save the selection to the LocalStorage drawer
+    localStorage.setItem('theme', theme);
 });
